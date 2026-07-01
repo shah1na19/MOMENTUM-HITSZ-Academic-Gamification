9e0a30a1c5e2e8c17bc2d3df0afe93b9d3be3035
Shahina <anihash9@gmail.com>
2026-07-01 12:37:17 +0800

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  /* Additional Next.js config options can go here if needed */
+};
+
+export default nextConfig;
