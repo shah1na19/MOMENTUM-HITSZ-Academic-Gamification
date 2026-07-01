19e6268b73173ae7e2e1ad0271640a6969e2124e
Shahina <anihash9@gmail.com>
2026-07-01 12:53:06 +0800

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  /* Additional Next.js config options can go here if needed */
-};
-
-export default nextConfig;
