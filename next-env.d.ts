745d9711287dac1f3a528950f9a6b14819281f8f
Shahina <anihash9@gmail.com>
2026-07-01 12:52:40 +0800

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// See https://nextjs.org/docs/basic-features/typescript for more information.
